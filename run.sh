b0433fb5d69ebaa15aaaa2f9074436e916a21015
Lawrence Nahum <lawrence@greenaddress.it>
2019-03-27 00:39:54 +0100

    currently disabled knots

--- run.sh
+++ run.sh
@@ -8,11 +8,7 @@ REPO_CORE=https://github.com/bitcoin/bitcoin.git
 DOCKERHASH=6603364284e4fe27f973e6d2e42b7eacf418baabf87b89638d46453772652d2e
 COMMIT_CORE=2472733a24a9364e4c6233ccd04166a26a68cc65
 
-REPO_KNOTS=https://github.com/bitcoinknots/bitcoin.git
-
-COMMIT_KNOTS=ab05daa871db7c5772e6477c0bdddaa6f3808afd
-
-repos="${REPO_CORE}_${COMMIT_CORE} ${REPO_KNOTS}_${COMMIT_KNOTS}"
+repos="${REPO_CORE}_${COMMIT_CORE}"
 for repo in ${repos}; do
   TARGETHOST=armv7a-linux-androideabi
   docker run -v $PWD:/repo $DOCKERBUILDER@sha256:$DOCKERHASH /bin/bash -c "/repo/fetchbuild.sh ${repo/_/ } $TARGETHOST 32" &
