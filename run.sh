68cc6bf5079c6f77cb595d40e1a67186c59b49db
Lawrence Nahum <lawrence@greenaddress.it>
2019-09-26 14:42:40 +0200

    update knots to 0.18.1.knots20190920

--- run.sh
+++ run.sh
@@ -14,7 +14,7 @@ build_repo() {
 }
 
 build_repo https://github.com/bitcoin/bitcoin.git 00ffe5aca134f424351b05a85da4eef9c3e4553a bitcoin bitcoin --disable-man
-build_repo https://github.com/bitcoinknots/bitcoin.git 5e1c2d13f506e58513064ecbd914e00a944ee6a0 bitcoin bitcoin --disable-man
+build_repo https://github.com/bitcoinknots/bitcoin.git f8d8a318e8ff7fb396b3102a532c790a7430ed81 bitcoin bitcoin --disable-man
 build_repo https://github.com/elementsproject/elements.git da7b8abda6f7cbe2ca1c8b7a038a969147e1c9cb elements liquid --enable-liquid
 
 wait
